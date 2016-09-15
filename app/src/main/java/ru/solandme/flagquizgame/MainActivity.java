f1b22b9512586e293a8ba6e243fdace4d9d41c71
Solomin Andrey <solomin.andrey@gmail.com>
2016-09-15 15:43:18 +0300

    Added layout fragment main

--- app/src/main/java/ru/solandme/flagquizgame/MainActivity.java
+++ app/src/main/java/ru/solandme/flagquizgame/MainActivity.java
@@ -1,11 +1,9 @@
 package ru.solandme.flagquizgame;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -36,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
